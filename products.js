cdeed427bc27495bab973db6a8159653205e24f6
chuchu-design <pink20020812@gmail.com>
2026-07-28 23:20:55 +0800

    Add files via upload

---
+++ products.js
@@ -0,0 +1,18 @@
+export const productCatalog = [
+    {
+        categoryId: "conan-30th",
+        categoryName: "名偵探柯南 30 週年紀念展",
+        items: [
+            {
+                id: "p30_lenticular_stand_osaka",
+                name: "大阪場光柵立牌盲抽",
+                image: "https://github.com/user-attachments/assets/c992b6c6-8509-48c9-9000-ea6319a743ad",
+                price: 300,
+                options: [
+                    "盲抽 (一抽) - NT$300",
+                    "端盒 (整盒) - NT$2350"
+                ]
+            }
+        ]
+    }
+];
